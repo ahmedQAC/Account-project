78057d953afafae9def1f6632b7b4c7a6a1793b9
AhmedQAC <ahmed.abidali@hotmail.com>
2019-01-16 10:38:51 +0000

    Added new account name counter functionality

--- AccountProject/src/main/java/com/qa/rest/Service.java
+++ AccountProject/src/main/java/com/qa/rest/Service.java
@@ -2,15 +2,31 @@ package com.qa.rest;
 import java.util.HashMap;
 import java.util.Map;
 
-public class Service {	
+public class Service {
+	private int id = 0;
 	private Map<Integer, Account> accountMap = new HashMap<>();
 
 	public Account getAccountMap(int key) {
 		return accountMap.get(key);
 	}
+	
+	public Map<Integer, Account> getTheWholeAccountMap() {
+		return accountMap;
+	}
 
-	public void addAccountMap(int key, Account account) {
-		accountMap.put(key, account);
+	public void addAccountMap(Account account) {
+		accountMap.put(id, account);
+		id++;
+	}
+	
+	public int accountNameCounter(String firstName) {
+		int counter = 0;
+		for (Account i : getTheWholeAccountMap().values()) {
+			if(i.getFirstName().equals(firstName)) {
+				counter++;
+			}
+		}
+		return counter;
 	}
 	
 }
